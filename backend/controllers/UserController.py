570dcec28be05a600bc613e649030a354479abf3
gabriel.stoffel <gabriel.stoffel@gmail.com>
2022-11-18 08:07:27 -0300

    commit

--- backend/controllers/UserController.py
+++ backend/controllers/UserController.py
@@ -2,19 +2,27 @@ from flask_api import status
 from flask import jsonify, request
 from models import User
 from database.database import db
+from sqlalchemy.exc import SQLAlchemyError
+from datetime import datetime
 
 def add_user():
     data = request.json
     try:
         user = User(
-            data['name'],
-            data['username'],
-            data['password']
+            data.get('name'),
+            data.get('username'),
+            data.get('password')
         )
-        print(user.serialize())
         db.session.add(user)
         db.session.commit()
         return  jsonify(user.serialize())
-    except Exception as err:
-	    return(str(err), status.HTTP_400_BAD_REQUEST)
+    except SQLAlchemyError as err:
+        response = {
+            'timestamp': datetime.now(),
+            'status': 400,
+            'error': 'Bad Request',
+            'message': str(err.__dict__['orig']),
+            'path': '/users'
+        }
+        return(jsonify(response), status.HTTP_400_BAD_REQUEST)
 
--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,13 @@
 from database.database import db
+from sqlalchemy.orm import backref
 
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), unique=True)
-    password = db.Column(db.String())
-    username = db.Column(db.String())
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(40), nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    username = db.Column(db.String(40), nullable=False)
 
     def __init__(self, name, username, password):
         self.name = name
@@ -22,3 +23,17 @@ class User(db.Model):
             'name': self.name,
             'username':self.username
         }
+
+class Project(db.Model):
+    __tablename__ = 'project'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    zip_code = db.Column(db.String(15), nullable=False)
+    cost = db.Column(db.Float, nullable=False)
+    done = db.Column(db.Boolean, default=False, nullable=False)
+    deadline = db.Column(db.DateTime, nullable=False)
+    username = db.Column(db.String(40), db.ForeignKey('user.username'), nullable=False)
+    user_project = db.relationship("User", backref=backref("user", uselist=False))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
